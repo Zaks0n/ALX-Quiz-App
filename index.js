2d4322b8453e826167947268e47170662c46ad90
Zaks0n <zaksontech@gmail.com>
2023-02-17 17:46:57 +0100

    Minor edit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function welcome() {
   console.log(`
     ${chalk.bgBlue('HOW TO PLAY')} 
     I am a process on your computer.
-    If you get any question wrong I will be ${chalk.bgRed('killed')}
+    If you get any question wrong I will be ${chalk.bgRed('terminated')}
     So get all the questions right...
 
   `);
